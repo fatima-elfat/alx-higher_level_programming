943c8372a8701d2ab30d76e77e06a9b14d88762c
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-08 20:52:23 +0000

    add task file

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(ord('z'), ord('a')-1, -1):
+    a = i
+    if i % 2 == 0
+        b = a - ord('a') + ord('A');
+        a = chr(b)
+    else:
+        a = chr(a)
+    print("{}".format(a), end='')
