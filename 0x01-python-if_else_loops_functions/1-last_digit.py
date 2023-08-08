4ac97869531c9239043cae40de3385be3b5f32e2
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-08 20:15:11 +0000

    add task file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 r = number
+r %= 10
 if number < 0:
     r *= -1
-r %= 10
 strr = "Last digit of {} is {}"
 if r > 5:
     strr += " and is greater than 5"
