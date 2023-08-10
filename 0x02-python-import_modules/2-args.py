e27400e5ea63987271d6f9657b869807ccb71e96
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-10 11:10:25 -0700

    add files of task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    a = argv
+    l_a = len(a)
+    print("{:d} {:s}".fortmat(l_a, 'argument.'), end="")
+    if l_a == 0:
+        print("{:s}".fortmat('s.'))
+    elif l_a == 1:
+        print("{:s}".fortmat(':'))
+    elif l_a > 1:
+        print("{:s}".fortmat('s:'))
+    for i in range(1, l_av + 1):
+        print('{:d}: {}'.format(i, a[i]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == '__main__':
+    r = 0
+    a = argv
+    l_a = len(a)
+    if (l_a > 1):
+        for i in range(1, l_a + 1):
+            r += (int(a[i]))
+    print("{:d}".format(r))
