bf15d1c1ce6f2129b315533ab88ef137a91ef2f3
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-12 06:29:20 -0700

    add files

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            l = len(i) - 1
+            if j == l:
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
+        print()
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    l_a = len(tuple_a)
+    l_b = len(tuple_b)
+    t_a = (tuple_a[0] if l_a > 0 else 0) + (tuple_b[0] if l_b > 0 else 0)
+    t_b = (tuple_a[1] if l_a > 1 else 0) + (tuple_b[1] if l_b > 1 else 0)
+    return (t_a, t_b)
