0dfc972afdfe9ce9582f1509226117672e6fd6af
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-14 09:04:09 -0700

    add file of advanced task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,20 @@
+#include <Python.h>
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: A PyObject list.
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t a, i, l_p;
+
+	a = ((PyListObject *)p)->allocated;
+	l_p = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", l_p);
+	printf("[*] Allocated = %ld\n", a);
+	for (i = 0; i < l_p; i++)
+	{
+		printf("Element %ld: %s\n",
+		       i,
+		       (PY_TYPE(PyList_GetItem(p, i)))->tp_name);
+	}
+}
