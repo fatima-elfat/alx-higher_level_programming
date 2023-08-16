394db1d564763fb2e8060f996c6646e97c4d188f
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-16 07:14:56 -0700

    add files of mandatory tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or len(a_dictionary) = 0:
+    if a_dictionary is None:
         return None
-    return max(a_dictionary, key=lambda a: a_dictionary[a])
+    return (max(a_dictionary, key=lambda a: a_dictionary[a]))
