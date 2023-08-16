f267c65ed8fb8bed903f3db82f670c735c3776bf
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-16 13:32:52 -0700

    add file of  task 13

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,4 @@
 #include "Python.h"
-#include "listobject.h"
-#include "object.h"
 #include <stdio.h>
 
 void print_python_bytes(PyObject *p)
@@ -17,11 +15,11 @@ void print_python_bytes(PyObject *p)
 	else
 	{
 		len = PyBytes_Size(p);
-		if ((len + 1) >= 10)
-			a = 10;
 		printf("  size: %d\n", len);
 		printf("  trying string: %s\n", py_c->ob_sval);
 		s = py_c->ob_sval;
+		if ((len + 1) >= 10)
+			a = 10;
 		printf("  first %d bytes: ", a);
 		while (a--)
 			printf(" %.2x", (unsigned char) *s++);
@@ -30,18 +28,19 @@ void print_python_bytes(PyObject *p)
 }
 void print_python_list(PyObject *p)
 {
-	int i, len = 0;
+	int i;
+	long long len;
 	PyListObject *py_c = (PyListObject *) p;
 	PyObject *it;
 
-	len = PyList_GET_SIZE(p);
+	len = (long long) PyList_Size(p);
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %lld\n", (long long) len);
+	printf("[*] Size of the Python List = %lld\n", len);
 	printf("[*] Allocated = %lld\n", (long long) py_c->allocated);
 	for (i = 0; i < len; ++i)
 	{
 		it = PyList_GET_ITEM(p, i);
-		printf("Element %lld: %s\n",(long long) i, it->ob_type->tp_name);
+		printf("Element %lld: %s\n", i, it->ob_type->tp_name);
 		if (PyBytes_Check(it))
 			print_python_bytes(it);
 	}
