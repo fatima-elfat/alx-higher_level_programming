be1bbb82ad56860fd0e4b2424ffcc1c49fc04643
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-16 06:40:45 -0700

    fix errors in files of mandatory tasks

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is None:
         return
-    it = = sorted(a_dictionary.items())
+    it = sorted(a_dictionary.items())
     for key, val in it:
         print("{}: {}".format(key, val))
