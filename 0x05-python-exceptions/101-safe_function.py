32588f3b5fb47bdf1ed308f841edaf6241b92bc6
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-28 05:13:11 -0700

    add advanced tasks

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (ValueError, TypeError, ZeroDivisionError, IndexError) as e:
+        sys.stderr.write("Exception: " + str(e) + "\n")
+        return None
