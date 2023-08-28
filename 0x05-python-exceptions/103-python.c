e474929d415a1524efb158c3915791637bcce526
fatima-elfat <ffzelfatehy@gmail.com>
2023-08-28 10:18:57 -0700

    fix compiling errors

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,33 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 #include "Python.h"
 
-void print_python_list(PyObject *p)
-{
-	Py_ssize_t  len, i;
-	PyListObject *py_c = (PyListObject *) p;
-	PyObject *it;
 
-	len = PyList_GET_SIZE(p);
-	printf("[*] Python list info\n");
-	if (!PyList_Check(p))
-	{
-		printf("  [ERROR] Invalid List Object\n");
-		return;
-	}
-	printf("[*] Size of the Python List = %ld\n", len);
-	printf("[*] Allocated = %ld\n", py_c->allocated);
-	for (i = 0; i < len; ++i)
-	{
-		it = py_c->ob_item[i];
-		printf("Element %ld: %s\n", i, it->ob_type->tp_name);
-		if (PyBytes_Check(it))
-			print_python_bytes(it);
-		else if (PyFloat_Check(it))
-			print_python_float(it);
-	}
-
-}
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t len = 0, i, len2;
@@ -68,9 +42,33 @@ void print_python_float(PyObject *p)
 		printf("  [ERROR] Invalid Float Object\n");
 		return;
 	}
-	i = py_c->ob_fval
+	i = py_c->ob_fval;
 	s = PyOS_double_to_string(i, 'r', 0,Py_DTSF_ADD_DOT_0, NULL);
 	printf("  value: %s\n", s);
 	free(s);
 }
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t  len, i;
+	PyListObject *py_c = (PyListObject *) p;
+	PyObject *it;
 
+	len = PyList_GET_SIZE(p);
+	printf("[*] Python list info\n");
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+	printf("[*] Size of the Python List = %ld\n", len);
+	printf("[*] Allocated = %ld\n", py_c->allocated);
+	for (i = 0; i < len; ++i)
+	{
+		it = py_c->ob_item[i];
+		printf("Element %ld: %s\n", i, it->ob_type->tp_name);
+		if (PyBytes_Check(it))
+			print_python_bytes(it);
+		else if (PyFloat_Check(it))
+			print_python_float(it);
+	}
+}
