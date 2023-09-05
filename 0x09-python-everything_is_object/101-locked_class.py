284afdbb1a16715dbc08b3a429429b98cea0add7
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-05 15:57:02 -0700

    fix errors

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -10,10 +10,4 @@ class LockedClass:
     AttributeError: 'LockedClass' object has no attribute 'last_name'.
     """
 
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            raise AttributeError(
-                    "'LockedClass' object has no attribute 'last_name'"
-                    )
-        else:
-            self.__dict__[name] = value
+    __slots__ = ["first_name"]
