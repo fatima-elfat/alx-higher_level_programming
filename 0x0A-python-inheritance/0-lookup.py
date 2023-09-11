57d1afd9cff95b3db4db4eb974f9863824f3b6ed
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-11 10:14:01 -0700

    add files

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+The model 0. Lookup.
+"""
+
+
+def lookup(obj):
+    """
+    returns the list of available attributes and methods of an object.
+    return: list object.
+    """
+    return dir(obj)
