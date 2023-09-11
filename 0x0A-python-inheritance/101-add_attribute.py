a76ec23dd7a09f4eadc43d7ae6d6412f6eb1a514
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-11 12:46:09 -0700

    fix var name

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,6 +7,6 @@ def add_attribute(obj, name, value):
     adds a new attribute to an object if it’s possible.
     """
     if ('__dict__' in dir(obj)):
-        setattr(obj, attribute, value)
+        setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
