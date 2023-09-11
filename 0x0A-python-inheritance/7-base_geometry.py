8226e998b3f18673581c2510061efd60ba7a1049
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-11 12:11:52 -0700

    fix errors

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,5 +24,5 @@ class BaseGeometry:
         """
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
+        if value < 1:
             raise ValueError("{:s} must be greater than 0".format(name))
