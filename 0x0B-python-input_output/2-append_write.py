d56e56da999425c2b5db900375ec489843746843
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-12 12:58:08 -0700

    fix check errors

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,10 +10,10 @@ def append_write(filename="", text=""):
     and returns the number of characters added.
 
     Args:
-        filename : The name of the file.
-        text : The text to append.
-
-    Return: the number of characters added.
+        filename : the name of the file.
+        text : the text to append.
+    Return:
+        the number of characters added.
     """
     with open(filename, mode="a", encoding="utf-8") as f:
         return(f.write(text))
