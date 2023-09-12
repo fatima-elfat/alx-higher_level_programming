039da7e8d3d732c72112e81610e131689d98153c
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-12 21:53:31 +0100

    fix check errors

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """
-the module of 2. Append to a file.
+
 """
 
 
 def append_write(filename="", text=""):
     """
-    
+    appends a string at the end of a text file.
+
+    Args:
+        filename : the name of the file.
+        text : the text to append.
+    Return:
+        the number of characters added.
     """
     with open(filename, mode="a", encoding="utf-8") as f:
         return(f.write(text))
