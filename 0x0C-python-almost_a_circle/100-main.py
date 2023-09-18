5b56552f3907924015409b5c46a935936e983bbc
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-18 22:19:42 +0100

    test

--- 0x0C-python-almost_a_circle/100-main.py
+++ 0x0C-python-almost_a_circle/100-main.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
 
     print("---")
     print("---")
-    
+
     s1 = Square(5)
     s2 = Square(7, 9, 1)
     list_squares_input = [s1, s2]
@@ -57,6 +57,16 @@ if __name__ == "__main__":
     s1 = Square(5, 1, 3, 3)
     s2 = Square(9, 5, 2, 3)
     Square.save_to_file_csv([s1, s2])
+    list_squares_output = Square.load_from_file_csv()
+    for square in list_squares_input:
+        print("[{}] {}".format(id(square), square))
+
+    print("---")
+
+    for square in list_squares_output:
+        print("[{}] {}".format(id(square), square))
+
+    Square.save_to_file_csv()
     list_squares_output = Square.load_from_file_csv()
     for square in list_squares_input:
         print("[{}] {}".format(id(square), square))
