3012786856e0b0fbb15738207a59be71cf3a9028
fatima-elfat <ffzelfatehy@gmail.com>
2023-09-18 22:18:09 +0100

    test

--- 0x0C-python-almost_a_circle/100-main.py
+++ 0x0C-python-almost_a_circle/100-main.py
@@ -23,7 +23,21 @@ if __name__ == "__main__":
 
     print("---")
     print("---")
+    r1 = Rectangle(10, 7, 2, 8, 1)
+    r2 = Rectangle(2, 4, 5, 6, 2)
+    Rectangle.save_to_file_csv([r1, r2])
+    list_rectangles_output = Rectangle.load_from_file_csv()
+    for rect in list_rectangles_input:
+        print("[{}] {}".format(id(rect), rect))
 
+    print("---")
+
+    for rect in list_rectangles_output:
+        print("[{}] {}".format(id(rect), rect))
+
+    print("---")
+    print("---")
+    
     s1 = Square(5)
     s2 = Square(7, 9, 1)
     list_squares_input = [s1, s2]
@@ -32,6 +46,18 @@ if __name__ == "__main__":
 
     list_squares_output = Square.load_from_file_csv()
 
+    for square in list_squares_input:
+        print("[{}] {}".format(id(square), square))
+
+    print("---")
+
+    for square in list_squares_output:
+        print("[{}] {}".format(id(square), square))
+    
+    s1 = Square(5, 1, 3, 3)
+    s2 = Square(9, 5, 2, 3)
+    Square.save_to_file_csv([s1, s2])
+    list_squares_output = Square.load_from_file_csv()
     for square in list_squares_input:
         print("[{}] {}".format(id(square), square))
 
