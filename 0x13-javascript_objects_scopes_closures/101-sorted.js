29e1d8ac5a32d173527d80f979a67165e56af8cf
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-14 20:24:10 +0100

    fix errors

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,12 +4,13 @@ const dict = require('./101-data').dict;
 
 const dikt = {};
 for (const key in dict) {
-  const listk = [];
+  //const listk = [];
   if (dikt[dict[key]] === undefined) {
     dikt[dict[key]] = [];
   }
-  listk.unshift(key);
-  dikt[dict[key]] = listk;
+  //listk.unshift(key);
+  //dikt[dict[key]] = listk;
+  dikt[dict[key]].push(key);
 }
 
 console.log(dikt);
