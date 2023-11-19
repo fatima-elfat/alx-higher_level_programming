f2117244523bfb14e0eb5631c5ebeba1e160ca5a
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-19 14:59:32 +0100

    add files of task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+the module of task 10.
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    db = argv[3]
+    user = argv[1]
+    passwd = argv[2]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    r = session.query(State).filter_by(name=argv[4]).first()
+    if r is None:
+        print("Nothing")
+    else:
+        print(r.id)
