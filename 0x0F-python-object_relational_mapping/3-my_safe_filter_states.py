3d022c2976ad8f48a2b75ae7dadd2cc93eaa5109
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-19 14:16:07 +0100

    add files of task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python3
+"""
+the module of task 1.
+"""
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-    q = "WHERE name LIKE BINARY {}".format("ff")
-    print("SELECT * FROM states  {}".format(q))
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        db=argv[3],
+        user=argv[1],
+        passwd=argv[2])
+    cr = db.cursor()
+    q = """SELECT * 
+    FROM states  
+    WHERE name=%s
+    """
+    cr.execute(q, (argv[4],))
+    for row in cr.fetchall():
+        print(row)
+    cr.close()
+    db.close()
