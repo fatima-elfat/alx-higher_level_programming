d422758914eda66932936922f089b04fdf07b37f
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-19 14:45:25 +0100

    add files of task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+the module of task 7.
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    db = argv[3]
+    user = argv[1]
+    passwd = argv[2]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for r in session.query(State).order_by(State.id):
+        print(r.id, r.name, sep=": ")
+    session.close()
