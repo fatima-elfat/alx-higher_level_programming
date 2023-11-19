ecf49a6c9d492904cc5089e6a7a4a104ac6e115d
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-19 14:26:48 +0100

    add files of task 4-5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+the module of task 1.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        db=argv[3],
+        user=argv[1],
+        passwd=argv[2])
+    cr = db.cursor()
+    q = """SELECT cities.name
+     FROM states
+     INNER JOIN cities ON states.id = cities.state_id
+     WHERE states.name LIKE %s
+     ORDER BY cities.id"""
+    cr.execute(q, (argv[4], ))
+    r = list(row[0] for row in cr.fetchall())
+    print(r, sep=", ")
+    cr.close()
+    db.close()
