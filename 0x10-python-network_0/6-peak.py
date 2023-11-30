9d7011014fe6768fd9bd7e635f64512ed2f05f52
fatima-elfat <ffzelfatehy@gmail.com>
2023-11-30 09:42:35 -0800

    fix errors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,7 @@ def find_peak(list_of_integers):
     finds a peak in a list of unsorted integers.
     based on binary search.
     """
-    midle = int(len(list_of_integers)//2)
+    midle = int((len(list_of_integers) + 1 )//2)
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
@@ -15,4 +15,4 @@ def find_peak(list_of_integers):
     if list_of_integers[0] < list_of_integers[len(list_of_integers) - 1]:
         return find_peak(list_of_integers[midle:])
     else:
-        return find_peak(list_of_integers[:midle + 1])
+        return find_peak(list_of_integers[:midle])
