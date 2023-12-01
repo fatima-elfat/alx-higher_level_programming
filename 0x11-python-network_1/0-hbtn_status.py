8f651339a412a945d6595f6dc47dee1e54f513f6
fatima-elfat <ffzelfatehy@gmail.com>
2023-12-01 12:54:20 +0100

    fix errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ from urllib import request
 if __name__ == "__main__":
     with request.urlopen("https://alx-intranet.hbtn.io/status") as req:
         resp = req.read()
-        print("Body resp:")
+        print("Body response:")
         print("\t- type: {}".format(type(resp)))
         print("\t- content: {}".format(resp))
         print("\t- utf8 content: {}".format(resp.decode("utf-8")))
