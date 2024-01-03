06ce9800d076721e25875d1ab1e78e6e06ce3841
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 17:35:43 +0100

    fix errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,5 @@ const id_ = process.argv[2];
 const request = require('request');
 request(url + id_, function (error, response, body) {
   if (error) console.log(error);
-  else if (response.statusCode === 200) {
-    const a = JSON.parse(body);
-    console.log(a.title);
-  } else console.log('Erorr Code:' + response.statusCode);
+  else console.log(JSON.parse(body).title);
 });
