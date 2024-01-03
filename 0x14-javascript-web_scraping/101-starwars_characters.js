0c7339360dbc79c0d57bdf7c814d31cd4efe67a6
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 19:37:26 +0100

    files of task 11

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const id_ = process.argv[2];
+const request = require('request');
+request(url + id_, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    const c = JSON.parse(body).characters;
+    getchars(0, c);
+  }
+});
+function getchars (i, c) {
+  request(c[i], function (error, response, body) {
+    if (error) console.log(error);
+    else {
+      console.log(JSON.parse(body).name);
+      if (c.length > i + 1) getchars(i + 1, c);
+    }
+  });
+}
