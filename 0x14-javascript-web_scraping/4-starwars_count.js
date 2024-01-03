8bcbc1fd2e6d6f7cffc21ead4209855f53d1b960
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 18:01:15 +0100

    files of task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    const movies = JSON.parse(body).results;
+    let count = 0;
+    for (const m in movies) {
+      const c = movies[m].c;
+      for (const p in c) {
+        if (c[p].includes('18')) count++;
+      }
+    }
+    console.log(count);
+  }
+});
