c51617856fd46e5e41b642edc2907e0056c1de9b
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 19:04:00 +0100

    files of task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const url = process.argv[2];
+const filename = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+request(url).pipe(fs.createWriteStream(filename));
