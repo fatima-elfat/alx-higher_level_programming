8be97b45f15fe3768828f47af826705133e92cd8
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 17:21:38 +0100

    files of task 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, function (error, response) {
-  if (error) console.log(err);
+  if (error) console.log(error);
   else console.log('code: ' + response.statusCode);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const url = 'http://swapi.co/api/films/';
+const id_ = process.argv[2];
+const request = require('request');
+request(url + id_, function (error, response, body) {
+  if (error) console.log(error);
+  else console.log(JSON.parse(body).title);
+});
