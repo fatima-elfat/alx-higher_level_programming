f7bd6657fea36ac0ea4e01617cbf9ed86b8df7f3
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 17:48:07 +0100

    fix errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const url = 'http://swapi.co/api/films/';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 const id_ = process.argv[2];
 const request = require('request');
 request(url + id_, function (error, response, body) {
