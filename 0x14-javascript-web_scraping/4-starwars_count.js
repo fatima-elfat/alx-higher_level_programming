ea6e70853fe62ba2cc933a85e18f3a5d6b7b8d80
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 18:09:34 +0100

    fix errors

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,10 +4,10 @@ const request = require('request');
 request(url, function (error, response, body) {
   if (error) console.log(error);
   else {
-    const movies = JSON.parse(body);
+    const movies = JSON.parse(body).results;
     let count = 0;
     for (const m in movies) {
-      const c = movies[m].c;
+      const c = movies[m].characters;
       for (const p in c) {
         if (c[p].includes('18')) count++;
       }
