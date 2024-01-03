80af10bc60d9f4fa6ecaa933fb7f04249efa5885
fatima-elfat <ffzelfatehy@gmail.com>
2024-01-03 19:17:26 +0100

    files of task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    const todos = JSON.parse(body);
+    const tasks = {};
+    for (const t in todos) {
+      if (todos[t].completed === true) {
+        if (tasks[todos[t].userId]) tasks[todos[t].userId]++;
+        else tasks[todos[t].userId] = 1;
+      }
+    }
+    console.log(tasks);
+  }
+});
